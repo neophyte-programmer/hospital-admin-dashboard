7da3bf7ab947caf4d8570587a485c0fa6780f7f5
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-12-11 21:59:59 +0000

    finished

--- app.js
+++ app.js
@@ -14,5 +14,6 @@ closeBtn.addEventListener("click", () => {
 themeToggler.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme-variables")
 
-    themeToggler.querySelector("i").classList.toggle("active")
+    themeToggler.querySelector("i:nth-child(1)").classList.toggle("active")
+    themeToggler.querySelector("i:nth-child(2)").classList.toggle("active")
 })
